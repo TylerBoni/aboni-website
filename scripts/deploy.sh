647c25078085ae4377348a85643c11907bf2bc4d
Tyler Boni <tboni@abonitech.com>
2024-08-17 12:08:37 -0700

    sectioned deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,33 +1,74 @@
 #!/bin/bash
 
+ACTION=$1
+HOSTNAME=$2
+
 # Function to handle errors and exit
 function handle_error {
   echo "Error: $1"
   exit 1
 }
 
-# Test SSH connection
-if ! ssh -o BatchMode=yes -o ConnectTimeout=5 $1 exit; then
-  handle_error "SSH connection to $1 failed."
-fi
+# Function to test SSH connection
+function test_ssh {
+  if ! ssh -o BatchMode=yes -o ConnectTimeout=5 "root@$HOSTNAME" true; then
+    handle_error "SSH connection to $HOSTNAME failed."
+  fi
+}
 
-# Build the app
-if ! sudo docker compose build app; then
-  handle_error "Docker Compose build failed."
-fi
+# Function to build the app
+function build_app {
+  if ! sudo docker compose build app; then
+    handle_error "Docker Compose build failed."
+  fi
+}
 
-# Load the image on the remote server
-if ! sudo docker save aboni/website | bzip2 | pv | ssh root@$1 docker load; then
-  handle_error "Failed to load Docker image on the remote server."
-fi
+# Function to save and load Docker image on remote server
+function deploy_image {
+  if ! sudo docker save aboni/website | bzip2 | pv | ssh "root@$HOSTNAME" docker load; then
+    handle_error "Failed to load Docker image on the remote server."
+  fi
+}
 
-# SSH into the remote server and restart the service using Docker Compose
-if ! ssh root@$1 << 'EOF'
-  cd /root/aboni-website || exit 1
-  docker compose up -d --no-deps app || exit 1
+# Function to restart the service using Docker Compose on the remote server
+function restart_service {
+  if ! ssh "root@$HOSTNAME" <<EOF
+    cd /root/aboni-website || exit 1
+    docker compose up -d --no-deps app || exit 1
 EOF
-then
-  handle_error "Failed to restart the service using Docker Compose."
-fi
+  then
+    handle_error "Failed to restart the service using Docker Compose."
+  fi
+}
 
-echo "Deployment completed successfully."
+# Main script logic to perform actions based on the first argument
+case $ACTION in
+  testconnect)
+    test_ssh
+    ;;
+  build)
+    build_app
+    ;;
+  deploy)
+    test_ssh
+    deploy_image
+    ;;
+  restart)
+    test_ssh
+    restart_service
+    ;;
+  full)
+    test_ssh
+    build_app
+    deploy_image
+    restart_service
+    ;;
+  *)
+    echo "Usage: $0 {tesconnect|build|deploy|restart|full} HOSTNAME"
+    exit 1
+    ;;
+esac
+
+echo "$ACTION completed successfully."
+
+
